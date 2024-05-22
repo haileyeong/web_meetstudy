1bc86ba7012d096dda6f49eb3c94354305843267
haileyeong <haileyeong@gmail.com>
2024-05-22 17:29:22 +0900

    Feat: Post Entity 작성

---
+++ src/main/java/com/elice/meetstudy/domain/post/domain/post.java
@@ -0,0 +1,18 @@
+package com.elice.meetstudy.domain.post.domain;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ *  Post domain.
+ */
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class post {
+
+}
